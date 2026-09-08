d13bda879a51fc2da06b994b7d9ff7b36c529894
Anugrah <anugrahdethe10@gmail.com>
2026-09-08 20:59:51 +0530

    Time: 20 ms (41.50%) | Memory: 12.4 MB (14.14%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,27 @@
+class Solution:
+    def reverse(self, x):
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        reversed_num = 0
+
+        while x > 0:
+            digit = x % 10
+            x //= 10
+
+            # Check overflow before multiplying by 10
+            if reversed_num > (INT_MAX - digit) // 10:
+                return 0
+
+            reversed_num = reversed_num * 10 + digit
+
+        reversed_num *= sign
+
+        # Final range check
+        if reversed_num < INT_MIN or reversed_num > INT_MAX:
+            return 0
+
+        return reversed_num
